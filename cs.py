702af0ccf7db2703f5a4c2fca1179aab78f94e6c
ksidharth <keesari64@gmail.com>
2025-05-10 18:21:02 +0530

    test 6

--- cs.py
+++ cs.py
@@ -6,4 +6,4 @@ def quick_sort(arr):
     middle = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
     return quick_sort(left) + middle + quick_sort(right)
-    abcds
+    abcdsjavs
