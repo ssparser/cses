0bafdf8aad8a16212f72b125ed11306d7de0c029
ksidharth <keesari64@gmail.com>
2025-05-10 17:51:37 +0530

    binary search

--- cs.py
+++ cs.py
@@ -1,6 +1,12 @@
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
